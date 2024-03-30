4a84401ca82e8849f37fe18288b7ae80f172aa90
aurelio <roko@DESKTOP-87BJPR2>
2024-03-30 15:26:03 +0100

    push 5

--- test/Arena.js
+++ test/Arena.js
@@ -1,28 +1,31 @@
 const { expect } = require("chai");
+describe("Token contract", function () 
+{
+	let owner;
+	let Arena;
 
-describe("Token contract", function () {
-    let owner;
-    let Arena;
+	before(async function () {
+		[owner,addr1] = await ethers.getSigners();
+		console.log("Owner's address:", owner.address);
+		Arena = await ethers.deployContract("AR", [1000]);
+	});
 
-    before(async function () {
-        [owner,addr1] = await ethers.getSigners();
-        console.log("Owner's address:", owner.address);
-        Arena = await ethers.deployContract("AR", [1000]);
+	it("Deployment should assign the total supply of tokens to the owner", async function () {
+		const ownerBalance = await Arena.balanceOf(owner.address);
+		expect(await Arena.totalSupply()).to.equal(ownerBalance);
+	});
 
-    });
+	it("Should create a new player", async function() {
+		await expect(Arena.createPlayer("roko")).to.not.be.reverted;
+		await expect(Arena.createPlayer("roko")).to.be.revertedWith("Player already exists with this address");
+		await expect(Arena.connect(addr1).createPlayer("roko")).to.be.revertedWith("Name already taken");
+		const player = await Arena.players(owner.address);
+		expect(player.userName).to.equal("roko");
+	});
 
-    it("Deployment should assign the total supply of tokens to the owner", async function () {
-        const ownerBalance = await Arena.balanceOf(owner.address);
-        expect(await Arena.totalSupply()).to.equal(ownerBalance);
-    });
+	it("Should create a new tournament", async function(){
 
-    it("Should create a new player", async function(){
-	    await expect(Arena.createPlayer("roko")).to.not.be.reverted;
-	    await expect(Arena.createPlayer("roko")).to.be.revertedWith("Player already exists with this address")
-	    await expect(Arena.connect(addr1).createPlayer("roko")).to.be.revertedWith("Name already taken")
-	    const player = await Arena.players(owner.address)
-	    expect(player.userName).to.equal("roko");
-    });
+	});
 
 });
 
